c501096189def797161791dde5a6503bb2bce26b
Rahul171201 <rahulroydps@gmail.com>
2023-02-20 11:21:59 +0530

    grid layout made

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
@@ -7,8 +6,16 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-   ok
-    </>
+    <div className={styles.mainWrapper}>
+        <div className={styles.gridWrapper}>
+          <div className={styles.profileCard}>profile card</div>
+          <div>work</div>
+          <div>play</div>
+          <div>study</div>
+          <div>exercise</div>
+          <div>social</div>
+          <div>self care</div>
+        </div>
+    </div>
   )
 }
